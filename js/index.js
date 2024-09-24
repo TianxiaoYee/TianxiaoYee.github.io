5c721956e7ac234a4ed63e9eee8f215afdbe80ad
fuwafuwaboom <ytxiao0109@gmail.com>
2024-09-23 22:36:15 -0500

    first commit

--- js/index.js
+++ js/index.js
@@ -9,3 +9,14 @@ $(document).ready(function() {
         $(this).find('img').css('display', 'inline-block');
     });
 })
+
+$(document).ready(function() {
+    $('.project-mousecell').mouseover(function() {
+        $(this).find('video').css('display', 'inline-block');
+        $(this).find('img').css('display', 'none');
+    });
+    $('.project-mousecell').mouseout(function() {
+        $(this).find('video').css('display', 'none');
+        $(this).find('img').css('display', 'inline-block');
+    });
+})
